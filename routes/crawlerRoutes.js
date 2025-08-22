9efe8772eab8dd0a5f180d9132aec31c1e26a1a1
Sachin Yadav <139240460+Sachinyadav00@users.noreply.github.com>
2025-08-22 11:48:51 +0530

    Delete routes directory

--- routes/crawlerRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
- 
-const router = express.Router();
-
-router.get("/urlsinsideme", urlsInsideMe);
-router.get("/urllinkedtome", urlLinkedToMe);
-
-module.exports = router;
